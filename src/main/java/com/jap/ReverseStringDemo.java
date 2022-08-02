6cc48344159446b27392a93294a7a532a79fa54d
Rohit Raj <rohit2000lmp@gmail.com>
2022-08-02 11:42:46 +0530

    s5pc1

--- src/main/java/com/jap/ReverseStringDemo.java
+++ src/main/java/com/jap/ReverseStringDemo.java
@@ -2,20 +2,20 @@ package com.jap;
 
 public class ReverseStringDemo {
     public static void main(String[] args) {
-
-
-
-        //Create a String object.
-
-        //Create an object of the class ReverseStringDemo
-
-        //Call the method getReverseString() and pass the parameter
-
-
+	String name = new String("Java Program"); //Create a String object.getReverseString(name);
+	System.out.println("Original String :" + name);
+	ReverseStringDemo object = new ReverseStringDemo();  //Create an object of the class ReverseStringDemo
+	String reverse = object.getReverseString(name);  //Call the method getReverseString() and pass the parameter
+	System.out.println(reverse);
     }
+
 //Write the logic to reverse the String  inside the below method and return the reverse String.
 public String getReverseString(String string){
-
-        return "";
+	int length = string.length();
+	String rev = "";
+	for(int i = length-1; i>=0; i--){
+		rev = rev + string.charAt(i);
+	}
+        return rev;
     }
 }
